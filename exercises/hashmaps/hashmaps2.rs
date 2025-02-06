7cc0b449d7c1761d3f9ec5bb833131aeef45d29b
r2coder <r2coder@163.com>
2025-02-07 07:27:23 +0800

    13/30

--- exercises/hashmaps/hashmaps2.rs
+++ exercises/hashmaps/hashmaps2.rs
@@ -14,7 +14,6 @@
 // Execute `rustlings hint hashmaps2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 use std::collections::HashMap;
 
@@ -25,6 +24,8 @@ enum Fruit {
     Mango,
     Lychee,
     Pineapple,
+    Strawberry,
+    Cherry,
 }
 
 fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
@@ -34,12 +35,15 @@ fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
         Fruit::Mango,
         Fruit::Lychee,
         Fruit::Pineapple,
+        Fruit::Strawberry,
+        Fruit::Cherry,
     ];
 
     for fruit in fruit_kinds {
-        // TODO: Insert new fruits if they are not already present in the
+        // Insert new fruits if they are not already present in the
         // basket. Note that you are not allowed to put any type of fruit that's
         // already present!
+        basket.entry(fruit).or_insert(2);
     }
 }
 
@@ -81,7 +85,7 @@ mod tests {
         let count = basket.values().sum::<u32>();
         assert!(count > 11);
     }
-    
+
     #[test]
     fn all_fruit_types_in_basket() {
         let mut basket = get_fruit_basket();
@@ -91,3 +95,4 @@ mod tests {
         }
     }
 }
+
--- exercises/quiz2.rs
+++ exercises/quiz2.rs
@@ -20,8 +20,6 @@
 //
 // No hints this time!
 
-// I AM NOT DONE
-
 pub enum Command {
     Uppercase,
     Trim,
@@ -32,11 +30,22 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function signature!
-    pub fn transformer(input: ???) -> ??? {
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
         // TODO: Complete the output declaration!
-        let mut output: ??? = vec![];
+        let mut output = vec![];
         for (string, command) in input.iter() {
             // TODO: Complete the function body. You can do it!
+            output.push(match command {
+                Command::Uppercase => string.to_uppercase(),
+                Command::Trim => string.trim().into(),
+                Command::Append(count) => {
+                    let mut s = string.clone();
+                    for _ in 0..*count {
+                        s.push_str("bar");
+                    }
+                    s
+                }
+            });
         }
         output
     }
@@ -45,8 +54,7 @@ mod my_module {
 #[cfg(test)]
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
-    use ???;
-    use super::Command;
+    use super::{my_module::transformer, Command};
 
     #[test]
     fn it_works() {
